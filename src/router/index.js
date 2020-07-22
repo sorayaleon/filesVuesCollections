b93e298c2adc17f0ef780e64945dd34ff10b192e
Soraya <soraya.l.g.88@gmail.com>
2020-07-22 20:50:55 +0200

    Components and Bulma

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,39 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Profile from '../views/Profile.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Recover from '../views/Recover.vue';
+
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/profile',
+    name: 'profile',
+    component: Profile,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+  },
+  {
+    path: '/recover',
+    name: 'recover',
+    component: Recover,
   },
 ];
 
