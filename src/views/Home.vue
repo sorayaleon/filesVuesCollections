7ab3c4c2df9cf8429d41589d43d964609374167a
Soraya <soraya.l.g.88@gmail.com>
2020-07-25 18:19:17 +0200

    Add resources

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,7 +44,7 @@
 
         <div class="column is-offset-1">
           <section class="section has-background-light">
-            <form>
+            <form @submit.prevent="add">
               <div class="field">
                 <label class="label">Resource title</label>
                 <div class="control">
@@ -66,12 +66,15 @@
                 </div>
               </div>
 
-              <div class="field is-grouped">
+              <div v-if="user" class="field is-grouped">
                 <div class="control">
-                  <button type="submit" class="button is-link">Add resource</button>
+                  <button type="submit" class="button is-link" :class="{'is-loading': working}">Add resource</button>
                 </div>
               </div>
+              <p v-else>To add resources you must <router-link to="/login">authenticate</router-link> to the app</p>
             </form>
+            <Error v-if="messageError" :message="messageError"></Error>
+            <Success @closeMessage="messageSuccess = ''" v-if="messageSuccess" :message="messageSuccess"></Success>
           </section>
         </div>
       </div>
@@ -80,11 +83,55 @@
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  components: {
-  },
-};
+  const fb = require('../firebase.js');
+  import Success from "../components/Success";
+  import Error from "../components/Error";
+  import { mapState } from 'vuex';
+  export default {
+    name: 'Home',
+    data(){
+      return {
+        title: '',
+        url: '',
+        description: '',
+        working: false,
+        messageError: '',
+        messageSuccess: '',
+      }
+    },
+    methods:{
+      add(){
+        this.working = true;
+        this.messageError = '';
+        fb.resourcesCollection.add({
+          when: new Date(),
+          title: this.title,
+          url: this.url,
+          description: this.description,
+          userId: this.user.uid,
+          name: this.profile.name,
+          comment: 0,
+          votes: 0,
+        }).then(() =>{
+          this.messageSuccess ='Successfully added resource.';
+          this.clear();
+        }).catch(error => {
+          console.error(error);
+          this.messageError(error.message);
+        }).finally(() => this.working = false);
+      },
+      clear() {
+        this.title = this.description = this.url = '';
+      },
+    },
+    computed:{
+      ...mapState(['user', 'profile']),
+    },
+    components: {
+      Success,
+      Error,
+    },
+  };
 </script>
 
 <style scoped>
